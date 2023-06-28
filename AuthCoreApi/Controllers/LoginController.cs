e4471692e8304a5027541c8191f2ba8e2d5f4e63
Jenis Patel <jenispatel181@gmail.com>
2023-06-28 11:38:41 -0400

    Add project files.

---
+++ AuthCoreApi/Controllers/LoginController.cs
@@ -0,0 +1,71 @@
+﻿using AuthCoreApi.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace AuthCoreApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : Controller
+    {
+        private IConfiguration _config;
+
+        public LoginController(IConfiguration config)
+        {
+            _config = config;
+        }
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult Login([FromBody] UserModel login)
+        {
+            IActionResult response = Unauthorized();
+            var user = AuthenticateUser(login);
+
+            if (user != null)
+            {
+                var tokenString = GenerateJSONWebToken(user);
+                response = Ok(new { token = tokenString });
+            }
+
+            return response;
+        }
+
+        private string GenerateJSONWebToken(UserModel userInfo)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(_config["Jwt:Issuer"],
+              _config["Jwt:Issuer"],
+              null,
+              expires: DateTime.Now.AddMinutes(120),
+              signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        private UserModel AuthenticateUser(UserModel login)
+        {
+            UserModel user = null;
+
+            //Validate the User Credentials    
+            //Demo Purpose, I have Passed HardCoded User Information    
+            if (login.Username == "Jenis")
+            {
+                user = new UserModel { Username = "Jenis Patel"};
+            }
+            return user;
+        }
+
+        [HttpGet]
+        [Authorize]
+        [Route("GetData")]
+        public string GetData()
+        {
+            return "you are authenticated using jwt";
+        }
+    }
+}
